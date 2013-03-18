2b70293ec94d8fb91dc3e63565c7e6b61e3dcde4
Hack Reactor Students <accounts@hackreactor.com>
2013-03-18 10:18:01 -0700

    puck creation

--- tracking.js
+++ tracking.js
@@ -8,4 +8,17 @@ window.onload = function() {
     puck.node.onclick = function() {
       puck.attr('cx', puck.attrs.cx - 10);
     };
+
+    var makePaddle = function(player) {
+      if(player === 1) {
+        var paddle = paper.rect(460, 200, 75, 10)
+      } else {
+        var paddle = paper.rect(460, 800, 75, 10)
+      }
+
+      return paddle;
+    };
+
+    paddle = makePaddle(1);
+    paddle = makePaddle(2);
 };
