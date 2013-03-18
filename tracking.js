7dde3a823aa1ae9f6fb46065fe5ee30d5d49006a
Tatiana Charkova <tattash@gmail.com>
2013-03-18 12:03:24 -0700

    Changed the board.

--- tracking.js
+++ tracking.js
@@ -1,24 +1,39 @@
 window.onload = function() {
-    var paper = new Raphael(document.getElementById('canvas_container'), 1000, 1000);
 
-    paper.path("M0,500L1000,500");
-    puck = paper.circle(500, 500, 10);
-    puck.attr({fill: '#333'});
+  var width = window.innerWidth;
+  var height = window.innerHeight;
 
-    puck.node.onclick = function() {
-      puck.attr('cx', puck.attrs.cx - 10);
-    };
+  var paper = new Raphael(document.getElementById('canvas_container'), width, height);
 
-    var makePaddle = function(player) {
-      if (player === 1) {
-        var paddle = paper.rect(460, 200, 75, 10);
-      } else if (player === 2) {
-        var paddle = paper.rect(460, 800, 75, 10);
-      }
+  paper.path("M"+width/2+",0L"+width/2+","+height);
+  paper.rect(0, 0, width/50, height/3).attr({fill:'#999'});
+  paper.rect(0, height*2/3, width/50, height/3).attr({fill:'#999'});
+  paper.rect(width-width/50, 0, width/50, height/3).attr({fill:'#999'});
+  paper.rect(width-width/50, height*2/3, width/50, height/3).attr({fill:'#999'});
 
-      return paddle;
-    };
+  paper.circle(width/50, height/2, height/2);
+  paper.circle(width-width/50, height/2, height/2);
+  paper.circle(width/2, height/2, height/5);
 
-    var paddle1 = makePaddle(1);
-    var paddle2 = makePaddle(2);
+  puck = paper.circle(width/2, height/2, height/25);
+  puck.attr({fill: '#333'});
+
+  puck.node.onclick = function() {
+    puck.attr('cx', puck.attrs.cx - 10);
+  };
+
+  var makePaddle = function(player) {
+    if (player === 1) {
+      var paddle = paper.circle(height/4, height/2, height/20);
+      paddle.attr({fill: '#F00'});
+    } else {
+      var paddle = paper.circle(width-height/4, height/2, height/20);
+      paddle.attr({fill: '#00F'});
+    }
+    return paddle;
+  };
+
+  var paddle1 = makePaddle(1);
+  var paddle2 = makePaddle(2);
+ 
 };
