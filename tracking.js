a2cd642c9a34e4fc4b5cf4413999a531b6d40324
Hack Reactor Students <accounts@hackreactor.com>
2013-03-18 16:27:10 -0700

    headtrack implemented, joined to paddle

--- tracking.js
+++ tracking.js
@@ -1,33 +1,33 @@
 window.onload = function() {
 
-  var width = window.innerWidth;
-  var height = window.innerHeight;
+  var width = 1500;
+  var height = 750;
 
   var paper = new Raphael(document.getElementById('canvas_container'), width, height);
 
-  paper.path("M"+width/2+",0L"+width/2+","+height);
-  paper.rect(0, 0, width/50, height/3).attr({fill:'#999'});
-  paper.rect(0, height*2/3, width/50, height/3).attr({fill:'#999'});
-  paper.rect(width-width/50, 0, width/50, height/3).attr({fill:'#999'});
-  paper.rect(width-width/50, height*2/3, width/50, height/3).attr({fill:'#999'});
+  paper.path("M"+ width /2 + ",0L" + width / 2 + "," + height);
+  paper.rect(0, 0, width / 50, height / 3).attr({fill:'#999'});
+  paper.rect(0, height * 2 / 3, width / 50, height / 3).attr({fill:'#999'});
+  paper.rect(width - width / 50, 0, width / 50, height / 3).attr({fill:'#999'});
+  paper.rect(width - width/ 50, height * 2 / 3, width / 50, height / 3).attr({fill:'#999'});
 
-  paper.circle(width/50, height/2, height/2);
-  paper.circle(width-width/50, height/2, height/2);
-  paper.circle(width/2, height/2, height/5);
+  paper.circle(width / 50, height / 2, height / 2);
+  paper.circle(width - width / 50, height / 2, height / 2);
+  paper.circle(width / 2, height / 2, height / 5);
 
-  puck = paper.circle(width/2, height/2, height/25);
+  puck = paper.circle(width / 2, height / 2, height / 25);
   puck.attr({fill: '#333'});
 
   puck.node.onclick = function() {
-    puck.attr('cx', puck.attrs.cx - 10);
+    puck.animate({cx:  puck.attrs.cx - 50}, 100);
   };
 
   var makePaddle = function(player) {
     if (player === 1) {
-      var paddle = paper.circle(height/4, height/2, height/20);
+      var paddle = paper.circle(height / 4, height / 2, height / 20);
       paddle.attr({fill: '#F00'});
-    } else {
-      var paddle = paper.circle(width-height/4, height/2, height/20);
+    } else if (player === 2) {
+      var paddle = paper.circle(width - height / 4, height / 2, height / 20);
       paddle.attr({fill: '#00F'});
     }
     return paddle;
@@ -35,4 +35,9 @@ window.onload = function() {
 
   var paddle1 = makePaddle(1);
   var paddle2 = makePaddle(2);
+
+  document.addEventListener('facetrackingEvent', function (event) {
+    paddle1.animate({cx: event.x * 4, cy: event.y * 2}, 25);
+  });
+
 };
