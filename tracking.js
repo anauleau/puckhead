11b93ffc44bc968c67f580170d624be698f8ec55
Hack Reactor Students <accounts@hackreactor.com>
2013-03-17 23:35:18 -0700

    basic svg in raphael

--- tracking.js
+++ tracking.js
@@ -1,17 +1,11 @@
-var canvas = document.createElement("canvas");
-var context = canvas.getContext("2d");
-canvas.width = 500;
-canvas.height =500;
-var color = '#333';
+window.onload = function() {
+    var paper = new Raphael(document.getElementById('canvas_container'), 1000, 1000);
 
-if (context) {
-    context.fillStyle = "rgb(200,0,0)";
-    context.fillRect(0, 0, 500, 250);
-    document.body.appendChild(canvas);
-}
+    paper.path("M0,500L1000,500")
+    puck = paper.circle(500, 500, 10);
+    puck.attr({fill: '#333'});
 
-var hero = {
-  speed: 256, // movement in pixels per second
-  x: canvas.width / 2,
-  y: canvas.height / 2
+    puck.node.onclick = function() {
+      puck.attr('cx', puck.attrs.cx - 10);
+    };
 };
