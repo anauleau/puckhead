532296528f4f45baff856dab80bd3dfcd39a9d45
Hack Reactor Students <accounts@hackreactor.com>
2013-03-18 10:23:45 -0700

    code clean

--- tracking.js
+++ tracking.js
@@ -1,7 +1,7 @@
 window.onload = function() {
     var paper = new Raphael(document.getElementById('canvas_container'), 1000, 1000);
 
-    paper.path("M0,500L1000,500")
+    paper.path("M0,500L1000,500");
     puck = paper.circle(500, 500, 10);
     puck.attr({fill: '#333'});
 
@@ -10,15 +10,15 @@ window.onload = function() {
     };
 
     var makePaddle = function(player) {
-      if(player === 1) {
-        var paddle = paper.rect(460, 200, 75, 10)
-      } else {
-        var paddle = paper.rect(460, 800, 75, 10)
+      if (player === 1) {
+        var paddle = paper.rect(460, 200, 75, 10);
+      } else if (player === 2) {
+        var paddle = paper.rect(460, 800, 75, 10);
       }
 
       return paddle;
     };
 
-    paddle = makePaddle(1);
-    paddle = makePaddle(2);
+    var paddle1 = makePaddle(1);
+    var paddle2 = makePaddle(2);
 };
