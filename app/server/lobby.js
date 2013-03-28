2a3455b8cee4ad8fe4a0f1340efb233eb067470b
Andreas Nauleau <anauleau@gmail.com>
2013-03-27 17:26:40 -0700

    WIP: fixing branch nonsense

--- app/server/lobby.js
+++ app/server/lobby.js
@@ -4,4 +4,9 @@ var rooms = {
   active: {}
 };
 
+var privateLobby = {
+  'http://localhost:8080/private':'http://localhost:8080/private'
+};
+
 module.exports.rooms = rooms;
+module.exports.privateLobby = privateLobby;
--- app/server/server.js
+++ app/server/server.js
@@ -8,8 +8,9 @@ var express = require('express'),
     User    = require('./user'),
     Room    = require('./room'),
     path    = require('path'),
-    uuid    = require('node-uuid');
-    physics = require('./serverPhysics');
+    uuid    = require('node-uuid'),
+    physics = require('./serverPhysics'),
+    u      = require('underscore'),
     users   = {};
 
 server.listen(8080);
@@ -19,36 +20,54 @@ app.get('/', function (req, res) {
   res.sendfile(path.resolve(__dirname + '/../client/index.html'));
 });
 
+app.get('/private', function (req, res) {
+  res.sendfile(path.resolve(__dirname + '/../client/index.html'));
+});
+
 io.sockets.on('connection', function (socket) {
 //create new user on connection - gives user socket property
+  var url;
+  socket.on('origin', function (data) {
+    url = data.url;
+    console.log("origin event triggered. socket id: " + socket.id);
+  });
+
   users[socket.id] = new User(socket);
   var user = users[socket.id];
 
-//conditional to deal with new users
-  //case one - new user joins existing room
-  if (lobby.rooms.waiting) {
-    var room = lobby.rooms.waiting;
-    room.user2 = user;
-    lobby.rooms.active[room.roomID] = room;
-
-    //assign each room's user with a room property
-    room.user1.room   = room.roomID;
-    room.user2.room   = room.roomID;
-
-    //assign each room's user an 'other' property to identify other room's user
-    room.user1.other  = room.user2.id;
-    room.user2.other  = room.user1.id;
-
-    //sets waitng to undefined
-    lobby.rooms.waiting = undefined;
-
-  //case two - else new room created for user, they are waiting
+  console.log(lobby.privateLobby);
+  console.log("starting private room. socket id: " + socket.id);
+  debugger;
+  if(u.contains(lobby.privateLobby, url)) {
+    console.log('werks');
   } else {
-    var room = new Room();
-    room.user1 = user;
-    room.user1.room = room.roomID;
-    lobby.rooms.waiting = room;
+    console.log('doesnt werks')
   }
+  //conditional to deal with new users
+    //case one - new user joins existing room
+    if (lobby.rooms.waiting) {
+      var room = lobby.rooms.waiting;
+      room.user2 = user;
+      lobby.rooms.active[room.roomID] = room;
+
+      //assign each room's user with a room property
+      room.user1.room   = room.roomID;
+      room.user2.room   = room.roomID;
+
+      //assign each room's user an 'other' property to identify other room's user
+      room.user1.other  = room.user2.id;
+      room.user2.other  = room.user1.id;
+
+      //sets waitng to undefined
+      lobby.rooms.waiting = undefined;
+
+    //case two - else new room created for user, they are waiting
+    } else {
+      var room = new Room();
+      room.user1 = user;
+      room.user1.room = room.roomID;
+      lobby.rooms.waiting = room;
+    }
 
   //On the Hi event,
   socket.on('join', function(data) {
@@ -58,7 +77,7 @@ io.sockets.on('connection', function (socket) {
   socket.on('playerReady', function(data){
     var thisRoom = lobby.rooms.active[user.room] || lobby.rooms.waiting;
     thisRoom.readyPlayers.push(data.player);
-
+    console.log('READY PLAYERS', thisRoom.readyPlayers);
     if (thisRoom.readyPlayers.length === 2) {
 
       //emits both players ready to each player from the other
@@ -98,3 +117,4 @@ io.sockets.on('connection', function (socket) {
 app.use(express.static(__dirname + '/../client'));
 app.use(express.static(__dirname + '/../styles'));
 app.use(express.static(__dirname + '/../vendor'));
+
